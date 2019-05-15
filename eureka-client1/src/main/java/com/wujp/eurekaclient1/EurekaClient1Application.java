4577f21f79da48be31fbd337e38db53752d3f820
wujianpeng <xiang163130@163.com>
2019-05-15 21:57:03 +0800

    eureka 注册中心简单实现

---
+++ eureka-client1/src/main/java/com/wujp/eurekaclient1/EurekaClient1Application.java
@@ -0,0 +1,29 @@
+package com.wujp.eurekaclient1;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableEurekaClient
+@SpringBootApplication
+@RestController
+public class EurekaClient1Application {
+
+    @Value("${server.port}")
+    String port;
+
+    @Value("${spring.application.name}")
+    String name;
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClient1Application.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "hello world from  "+ name + ":" + port;
+    }
+}
