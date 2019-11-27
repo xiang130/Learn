abbdaa32a6f3e68ed540ee1b54679df2e5f87efc
jianpeng Wu <xiang163130@163.com>
2019-11-27 22:10:20 +0800

    init

---
+++ src/test/java/com/wujp/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wujp.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
